f42aea3846eff1d69e0ab51d565c246b899ca849
cerminatop <Pablo.Cerminato@dowjones.com>
2020-04-26 21:02:07 -0300

    Adding App.js and index.html

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello from React-Boilerplate</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
